b1993fca69bb8f9580900df144588b5be2a188c1
KayoLino <kayofelipelino2002@gmail.com>
2024-08-05 12:41:33 -0300

    Criando sideBar da HomePage Profile

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,23 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './HomePage.css';
 import { useNavigate, useParams } from 'react-router-dom';
 import { isAuthenticated } from '../../components/auth';
+import Profile from '../../assets/images/profile.png';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
 
   const navigate = useNavigate();
   const { id } = useParams();
+  const [username, setUsername] = useState('');
 
   useEffect(() => {
+
+    const usersList = JSON.parse(localStorage.getItem('users')) || [];
+    const user = usersList.find(user => user.id === parseInt(id));
+    if (user) {
+      setUsername(user.username);
+    }
+
     if (!isAuthenticated()) {
       navigate('/login');
     }
-  }, [navigate]);
+  }, [navigate, id]);
 
   return (
-    <div>
-      <h1>Você logou</h1>
-      <p>ID do usuário: {id}</p>
+    <div className="sidebar ">
+      <div className="conteiner-sidebar">
+        <div className="logo">
+          <i class='bx bx-crown'></i>
+          <div>T<span>a</span>skPulse</div>
+        </div>
+
+        <div className="profile">
+          <img src={Profile} alt="" />
+          <div>{username}</div>
+        </div>
+
+        <ul className="menu-sidebar">
+          <li><Link><i class='bx bx-user-circle'></i><span>Editar Perfil</span> </Link></li>
+          <li><Link><i class='bx bx-cog' ></i><span>Configurações</span></Link></li>
+          <li><Link><i class='bx bx-info-circle' ></i><span>Ajuda e Suporte</span></Link></li>
+        </ul>
+
+        <ul className="menu-sidebar logout">
+          <li><Link><i class='bx bx-log-out-circle' ></i> <span>Sair</span></Link></li>
+        </ul>
+
+      </div>
     </div>
   );
 }
